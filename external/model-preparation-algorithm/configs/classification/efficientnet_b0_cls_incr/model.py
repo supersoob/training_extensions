edcd379f2d21fa580816d453f9f8d63298979b75
Lee, Soobee <soobee.lee@intel.com>
2022-08-23 17:49:08 +0900

    enable early stop for exp

--- external/model-preparation-algorithm/configs/classification/efficientnet_b0_cls_incr/model.py
+++ external/model-preparation-algorithm/configs/classification/efficientnet_b0_cls_incr/model.py
@@ -1,4 +1,17 @@
 _base_ = [
   '../../../submodule/models/classification/ote_efficientnet_b0.yaml',
 ]
+runner = dict(max_epochs=90)
 fp16 = dict(loss_scale=512.)
+
+custom_hooks = [
+    dict(
+        type='LazyEarlyStoppingHook',
+        start=0,
+        patience=10,
+        iteration_patience=0,
+        metric='mAP',
+        interval=1,
+        priority=75,
+    ),
+]
--- external/model-preparation-algorithm/configs/classification/mobilenet_v3_large_1_cls_incr/model.py
+++ external/model-preparation-algorithm/configs/classification/mobilenet_v3_large_1_cls_incr/model.py
@@ -1,4 +1,18 @@
 _base_ = [
   '../../../submodule/models/classification/ote_mobilenet_v3_large.yaml',
 ]
+
+runner = dict(max_epochs=90)
 fp16 = dict(loss_scale=512.)
+
+custom_hooks = [
+    dict(
+        type='LazyEarlyStoppingHook',
+        start=0,
+        patience=10,
+        iteration_patience=0,
+        metric='mAP',
+        interval=1,
+        priority=75,
+    ),
+]
