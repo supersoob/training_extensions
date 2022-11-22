344065197528527193d1d23e270ed16c7bb7fa28
Kim, Sungchul <sungchul.kim@intel.com>
2022-11-22 13:30:47 +0900

    update cfg to convert to_rgb to False
    **It is required to automatically convert it in the source code

--- otx/algorithms/classification/configs/base/data/selfsl_data_224.py
+++ otx/algorithms/classification/configs/base/data/selfsl_data_224.py
@@ -3,7 +3,8 @@
 __resize_target_size = 224
 __img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375]
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True
 )
 
 __train_pipeline_v0 = [
