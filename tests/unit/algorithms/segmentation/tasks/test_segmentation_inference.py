0051574981a85ef78b87b7d36c352940ac18098a
Lee, Soobee <soobeele@intel.com>
2023-01-28 02:03:55 +0900

    inference.py

--- tests/unit/algorithms/segmentation/tasks/test_segmentation_inference.py
+++ tests/unit/algorithms/segmentation/tasks/test_segmentation_inference.py
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import copy
+import glob
 import os
-import random
 import warnings
 
 import numpy as np
 import pytest
 
-from otx.algorithms.segmentation.tasks import SegmentationTrainTask
+from otx.algorithms.segmentation.tasks import SegmentationInferenceTask
 from otx.api.configuration.configurable_parameters import ConfigurableParameters
 from otx.api.configuration.helper import create
 from otx.api.entities.annotation import (
@@ -23,14 +24,14 @@ from otx.api.entities.datasets import DatasetEntity
 from otx.api.entities.image import Image
 from otx.api.entities.label import Domain, LabelEntity
 from otx.api.entities.label_schema import LabelGroup, LabelGroupType, LabelSchemaEntity
-from otx.api.entities.metrics import NullPerformance
 from otx.api.entities.model import ModelConfiguration, ModelEntity
 from otx.api.entities.model_template import parse_model_template
+from otx.api.entities.resultset import ResultSetEntity
 from otx.api.entities.shapes.ellipse import Ellipse
 from otx.api.entities.shapes.polygon import Point, Polygon
 from otx.api.entities.shapes.rectangle import Rectangle
-from otx.api.entities.subset import Subset
 from otx.api.entities.task_environment import TaskEnvironment
+from otx.api.usecases.tasks.interfaces.export_interface import ExportType
 from tests.test_suite.e2e_test_system import e2e_pytest_unit
 from tests.unit.api.test_helpers import generate_random_annotated_image
 
@@ -64,7 +65,6 @@ class TestOTXSegTaskTrain:
     def init_environment(self, params, model_template, number_of_images=10):
         labels_names = ("rectangle", "ellipse", "triangle")
         labels_schema = self.generate_label_schema(labels_names)
-        labels_list = labels_schema.get_labels(False)
         environment = TaskEnvironment(
             model=None,
             hyper_parameters=params,
@@ -72,7 +72,13 @@ class TestOTXSegTaskTrain:
             model_template=model_template,
         )
 
+        return environment
+
+    def generate_dataset(self, number_of_images=10):
         items = []
+        labels_names = ("rectangle", "ellipse", "triangle")
+        labels_schema = self.generate_label_schema(labels_names)
+        labels_list = labels_schema.get_labels(False)
         for i in range(0, number_of_images):
             image_numpy, shapes = generate_random_annotated_image(
                 image_width=640,
@@ -108,22 +114,9 @@ class TestOTXSegTaskTrain:
             items.append(DatasetItemEntity(media=image, annotation_scene=annotation))
         warnings.resetwarnings()
 
-        rng = random.Random()
-        rng.shuffle(items)
-        for i, _ in enumerate(items):
-            subset_region = i / number_of_images
-            if subset_region >= 0.8:
-                subset = Subset.TESTING
-            elif subset_region >= 0.6:
-                subset = Subset.VALIDATION
-            else:
-                subset = Subset.TRAINING
-
-            items[i].subset = subset
-
         dataset = DatasetEntity(items)
 
-        return environment, dataset
+        return dataset
 
     @pytest.fixture(autouse=True)
     def setup(self) -> None:
@@ -131,32 +124,41 @@ class TestOTXSegTaskTrain:
         hyper_parameters = create(model_template.hyper_parameters.data)
         hyper_parameters.learning_parameters.num_iters = 1
         hyper_parameters.learning_parameters.batch_size = 5
-        task_env, self.dataset = self.init_environment(hyper_parameters, model_template, 5)
-        self.seg_train_task = SegmentationTrainTask(task_env)
+        task_env = self.init_environment(hyper_parameters, model_template)
+        self.seg_train_task = SegmentationInferenceTask(task_env)
         model_configuration = ModelConfiguration(
             configurable_parameters=ConfigurableParameters(header="header", description="description"),
             label_schema=LabelSchemaEntity(),
         )
         self.model = ModelEntity(train_dataset=DatasetEntity(), configuration=model_configuration)
-        self.model_ckpt_path = self.seg_train_task._output_path
+        self.output_path = self.seg_train_task._output_path
 
     @e2e_pytest_unit
-    def test_save_model(self, mocker):
-        mocker.patch("torch.load", return_value="")
-        self.seg_train_task.save_model(self.model)
+    def test_infer(self):
+        dataset = self.generate_dataset(5)
+        anns = {}
+        for dataset_item in dataset:
+            anns[dataset_item.roi.id] = copy.deepcopy(dataset_item.annotation_scene)
 
-        assert self.model.get_data("weights.pth")
-        assert self.model.get_data("label_schema.json")
+        updated_dataset = self.seg_train_task.infer(dataset, None)
 
-    @e2e_pytest_unit
-    def test_train(self, mocker):
-        mocker.patch.object(SegmentationTrainTask, "save_model")
-        self.seg_train_task.train(self.dataset, self.model)
-
-        assert self.model.performance != NullPerformance()
+        for updated in updated_dataset:
+            assert len(updated.annotation_scene.annotations) > len(anns[updated.roi.id].annotations)
 
     @e2e_pytest_unit
-    def test_cancel_training(self):
-        self.seg_train_task.cancel_training()
+    def test_evaluate(self):
+        result_set = ResultSetEntity(
+            model=self.model,
+            ground_truth_dataset=DatasetEntity(),
+            prediction_dataset=DatasetEntity(),
+        )
+        with pytest.raises(ValueError):
+            self.seg_train_task.evaluate(result_set)
 
-        assert self.seg_train_task._should_stop is True
+    def test_export(self):
+        self.seg_train_task.export(ExportType.OPENVINO, self.model)
+
+        assert self.model.get_data("openvino.bin")
+        assert self.model.get_data("openvino.xml")
+        assert glob.glob(os.path.join(self.output_path, "**/model.xml"))
+        assert glob.glob(os.path.join(self.output_path, "**/model.bin"))
