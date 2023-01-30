b21f8d102d81ea931e2acd7e228535c761140001
Lee, Soobee <soobeele@intel.com>
2023-01-30 23:08:46 +0900

    test

--- tests/unit/algorithms/segmentation/mpa/seg/test_seg_stage.py
+++ tests/unit/algorithms/segmentation/mpa/seg/test_seg_stage.py
@@ -1,11 +1,11 @@
-
 from otx.mpa.seg.stage import SegStage
+
 
 class TestOTXSegStage:
     @pytest.fixture(autouse=True)
     def setup(self) -> None:
         default_config = "./otx/recipes/stages/segmentation/incremental.py"
-        self.stage = SegStage(name="",mode="train", config=default_config, common_cfg=None, index = 0)
+        self.stage = SegStage(name="", mode="train", config=default_config, common_cfg=None, index=0)
 
     @e2e_pytest_unit
     def configure(self, mocker):
