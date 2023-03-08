01f0d90bf6e83fa43b29899274a0026a37c92df1
Sungchul Kim <sungchul.kim@intel.com>
2023-03-08 15:16:48 +0900

    update script

---
+++ script/aie/train_poa_clip_vit_freeze.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    # command_build = (
+    #     "otx build Custom_Image_Classification_EfficinetNet-B0 "
+    #     "--backbone mmcls.VisionTransformer "
+    #     "--work-dir otx-workspace-AIE/POA_attribution")
+
+    # subprocess.run(command_build, shell=True)
+
+    GPU = 2
+    os.makedirs("otx-workspace-AIE/POA_attribution/clip_vit_freeze", exist_ok=True)
+    command_train = (
+        f"CUDA_VISIBLE_DEVICES={GPU} otx train "
+        f"--save-model-to /local/sungchul/logs/aie/poa/clip_vit_freeze "
+        f"--work-dir otx-workspace-AIE/POA_attribution/clip_vit_freeze "
+        f"2>&1 | tee otx-workspace-AIE/POA_attribution/clip_vit_freeze/output.log")
+
+    subprocess.run(command_train, shell=True)
