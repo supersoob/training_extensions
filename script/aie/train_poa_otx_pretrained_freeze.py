08e1a62cbc9f72bb11a65a85901e5f7d778d0ca3
Sungchul Kim <sungchul.kim@intel.com>
2023-03-08 14:46:32 +0900

    add script

---
+++ script/aie/train_poa_otx_pretrained_freeze.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    # command_build = (
+    #     "otx build Custom_Image_Classification_EfficinetNet-B0 "
+    #     "--backbone mmcls.VisionTransformer "
+    #     "--work-dir otx-workspace-AIE/POA_attribution")
+
+    # subprocess.run(command_build, shell=True)
+
+    GPU = 1
+    os.makedirs("otx-workspace-AIE/POA_attribution/otx_pretrained_freeze", exist_ok=True)
+    command_train = (
+        f"CUDA_VISIBLE_DEVICES={GPU} otx train "
+        f"--save-model-to /local/sungchul/logs/aie/poa/otx_pretrained_freeze "
+        f"--work-dir otx-workspace-AIE/POA_attribution/otx_pretrained_freeze "
+        f"2>&1 | tee otx-workspace-AIE/POA_attribution/otx_pretrained_freeze/output.log")
+
+    subprocess.run(command_train, shell=True)
